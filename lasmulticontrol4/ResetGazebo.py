243a9562880e6bcd50c1f4d9353c0ff777d52679
Pytrik1 <pytrik.v.rosendal@gmail.com>
2020-03-16 00:13:26 +0100

    corona

--- lasmulticontrol4/ResetGazebo.py
+++
@@ -1,8 +0,0 @@
-import rospy
-from std_srvs.srv import Empty
-
-# maybe do some 'wait for service' here
-reset_simulation = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
-
-# invoke
-reset_simulation()
