b4807c6e5995aaf51e6c59b11b1909c3a1c04aea
Pytrik1 <pytrik.v.rosendal@gmail.com>
2020-03-20 00:55:51 +0100

    params update

--- lasmultigazebo4/config/params.py
+++ lasmultigazebo4/config/params.py
@@ -4,19 +4,19 @@ import numpy as np
 #Controller gains
 c = np.float32(0.5)
 cf = np.float(1.2)
-cD =np.float(20)
+cD =np.float(2.0)
 cI = np.float(0.6)
 cP = np.float(4)
 calpha = np.float(40)
 czeta = np.float(4)
 
 #Goal position of centroid
-pGoal = np.array([[-1.0],[-1.0]])
-r_safe = np.float32(0.6)
+pGoal = np.array([[-2.0],[-2.0]])
+r_safe = np.float32(0.8)
 
 v_min = 0.1
 v_max = 0.5
-Ug_lim = 0.2
+Ug_lim = 0.25
 h = 0.005
 
 cO = 5
